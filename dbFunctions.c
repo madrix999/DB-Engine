8d7fb303f80d14c10f598a5ed731275cb1c7c878
William Morel <morelwilliam@outlook.com>
2016-11-05 22:31:28 -0500

    Initial commit.

---
+++ dbFunctions.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+#include "Functions.h"
+
+int init() {
+	bool success = false;
+	
+	return success;
+}
+
+void close() {
+	
+}
--- dbFunctions.h
+++ dbFunctions.h
@@ -1,4 +1,8 @@
 #ifndef DBENGINE_FUNCTIONS_H
 #define DBENGINE_FUNCTIONS_H
 
+int init();
+
+void close();
+
 #endif
