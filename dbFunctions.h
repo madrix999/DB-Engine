630d552cac02f2cb781c391f0ff10c837035f621
William Morel <morelwilliam@outlook.com>
2017-09-02 20:40:08 -0400

    Initial Commit

--- dbFunctions.h
+++ dbFunctions.h
@@ -18,16 +18,7 @@ typedef struct child {
 	char *element;
 } node_c;
 
-
-void print_parent(node_p * parent, node_c * child);
-void add_parent_node(node_p * parent, int id, char *element);
-void add_child(node_c * child, int var, char *element, int itemid, int parentid);
-void init(node_p * parent, node_c * child);
-int commands(char *cmd, node_p * parent, node_c * child);
-int check_parents(node_p * parent, char *arg);
+int init();
 char *input();
-
-void cmdParent(node_p * parent);
-void cmdChild(node_c * child);
 
 #endif
