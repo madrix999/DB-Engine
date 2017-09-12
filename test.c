f13b728423e1588845b8d01a643c2e2713ce2ae1
madrix999 <william@T530.int.crosemont.qc.ca>
2017-09-12 07:55:05 -0400

    added test.c (note file)

---
+++ test.c
@@ -0,0 +1,8 @@
+int function(char *file) {
+	int pos=0;
+	int line=0;
+
+	// run through first line, which will parse how many arguments+the names
+	// run through the second line, which will parse the data types required for the arguments, which will be used to create head.
+	// run through all the lines, char by char, set each line as a node, first one attached to head, rest attached to the last one initiated
+}
